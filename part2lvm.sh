db69f5eb8c8d65b4c445b0f90c6033bfc9de7a09
mhard666 <mirko.haertwig@gmx.de>
2021-01-14 20:14:54 +0100

    testdaten auskommentiert

--- part2lvm.sh
+++ part2lvm.sh
@@ -249,20 +249,20 @@ lvmLogicalVolumeData='lv_root 10G ext4 / /mnt/dst
 lv_swap 16G swap
 lv_home 20G ext4 /home /mnt/dst/home
 lv_opt 2G ext4 /opt /mnt/dst/opt
-lv_var 5G ext4 /var /mnt/dst/var
+lv_var 10G ext4 /var /mnt/dst/var
 lv_var_log 5G ext4 /var/log /mnt/dst/var/log
 lv_var_tmp 5G ext4 /var/tmp /mnt/dst/var/tmp
 lv_var_lib_postgresql 40G ext4 /var/lib/postgresql /mnt/dst/var/lib/postgresql'
 
 #zum testen...
-lvmLogicalVolumeData='lv_root 3G ext4 / /mnt/dst
-lv_swap 1G swap
-lv_home 2G ext4 /home /mnt/dst/home
-lv_opt 1G ext4 /opt /mnt/dst/opt
-lv_var 2G ext4 /var /mnt/dst/var
-lv_var_log 2G ext4 /var/log /mnt/dst/var/log
-lv_var_tmp 2G ext4 /var/tmp /mnt/dst/var/tmp
-lv_var_lib_postgresql 2G ext4 /var/lib/postgresql /mnt/dst/var/lib/postgresql'
+#lvmLogicalVolumeData='lv_root 3G ext4 / /mnt/dst
+#lv_swap 1G swap
+#lv_home 2G ext4 /home /mnt/dst/home
+#lv_opt 1G ext4 /opt /mnt/dst/opt
+#lv_var 2G ext4 /var /mnt/dst/var
+#lv_var_log 2G ext4 /var/log /mnt/dst/var/log
+#lv_var_tmp 2G ext4 /var/tmp /mnt/dst/var/tmp
+#lv_var_lib_postgresql 2G ext4 /var/lib/postgresql /mnt/dst/var/lib/postgresql'
 
 # Variable zur Zeilenweisen Aufbereitung der Ergebnisse aus dem vorhergehenden Loop zur Weiterverarbeitung im nächsten Loop
 nextLoop=""
