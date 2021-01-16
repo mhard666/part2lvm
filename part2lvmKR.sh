b103da692eb7c169862a88c0f743eeef5ca8cc30
mhard666 <mirko.haertwig@gmx.de>
2021-01-16 11:45:01 +0100

    testdaten aktiviert

--- part2lvmKR.sh
+++ part2lvmKR.sh
@@ -255,13 +255,13 @@ lv_var_tmp 5G ext4 /var/tmp /mnt/dst/var/tmp
 lv_var_lib_postgresql 40G ext4 /var/lib/postgresql /mnt/dst/var/lib/postgresql'
 
 #zum testen...
-#lvmLogicalVolumeData='lv_swap 1G swap
-#lv_home 2G ext4 /home /mnt/dst/home
-#lv_opt 1G ext4 /opt /mnt/dst/opt
-#lv_var 2G ext4 /var /mnt/dst/var
-#lv_var_log 2G ext4 /var/log /mnt/dst/var/log
-#lv_var_tmp 2G ext4 /var/tmp /mnt/dst/var/tmp
-#lv_var_lib_postgresql 2G ext4 /var/lib/postgresql /mnt/dst/var/lib/postgresql'
+lvmLogicalVolumeData='lv_swap 1G swap
+lv_home 2G ext4 /home /mnt/dst/home
+lv_opt 1G ext4 /opt /mnt/dst/opt
+lv_var 2G ext4 /var /mnt/dst/var
+lv_var_log 2G ext4 /var/log /mnt/dst/var/log
+lv_var_tmp 2G ext4 /var/tmp /mnt/dst/var/tmp
+lv_var_lib_postgresql 2G ext4 /var/lib/postgresql /mnt/dst/var/lib/postgresql'
 
 # Variable zur Zeilenweisen Aufbereitung der Ergebnisse aus dem vorhergehenden Loop zur Weiterverarbeitung im nächsten Loop
 nextLoop=""
