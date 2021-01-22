a18a81d7c908dd8a9bbb8b6457a9c0475f89f371
mhard666 <mirko.haertwig@gmx.de>
2021-01-22 20:16:14 +0100

    umountFstab hinzugefügt, anpassungen

--- test-IterateFstab.sh
+++ test-IterateFstab.sh
@@ -109,10 +109,10 @@ function mountFstab {
 
     # Alle Einträge der /etc/fstab liefern, die mit U(UID) oder /(dev/...) beginnen und Filesystem
     # ext2, ext3 oder ext4 sind.
-    # line=$(grep ^[U\/] /etc/fstab | grep '.ext[2-4].')
+    line=$(grep -v '^#' "$mF_fstab" | grep -v '.swap.' | grep -v 'udf' | grep -v 'iso9660' | awk '{print $2}' | sort)
 
     # Alle Einträge der /etc/fstab liefern, ausser Kommentare, swap, iso9660, udf
-    line=$(grep -v '^#' "$mF_fstab" | grep -v '.swap.' | grep -v 'udf' | grep -v 'iso9660')
+    # line=$(grep -v '^#' "$mF_fstab" | grep -v '.swap.' | grep -v 'udf' | grep -v 'iso9660')
     
     # Effektiv kann die liste noch sortiert werden nach Spalte 2 (Mountpoint)
     # line=$(grep -v '^#' /etc/fstab | grep -v '.swap.' | grep -v 'udf' | grep -v 'iso9660' | sort -k 2)
@@ -126,34 +126,109 @@ function mountFstab {
         echo $entry 
 
         # in Einzelbestandteile zerlegen
-        uuid=""
-        device=$(echo $entry | awk '{print $1}')    # Gerät
-        mountpoint=$(echo $entry | awk '{print $2}')    # Mountpoint
-        filesystem=$(echo $entry | awk '{print $3}')    # Filesystem
+        # $entry liefert den Mountpoint, Eintrag in der fstab suchen und diese Zeile zurückgeben
+        r=$(grep "$entry " "$mF_fstab")
 
-        echo $device
-        echo $mountpoint
-        echo $filesystem
+        # Prüfen, ob $r kein leerer String ist
+        if [ "r" != "" ]; then
 
-        uuid=$(echo ${device} | grep '^UUID' | cut -d = -f 2)   # Geräte mit UUID, UUID separieren
-        echo $uuid
-        if [ "$uuid" != "" ]; then
+#            umount "$mp"
 
-            # wenn $uuid einen Wert enthält (eine UUID) -> prüfen, ob für diese UUID ein Gerät existiert
-            if [ -h /dev/disk/by-uuid/$uuid ]; then
+            uuid=""
+            device=$(echo $r | awk '{print $1}')    # Gerät
+            mountpoint=$(echo $r | awk '{print $2}')    # Mountpoint
+            filesystem=$(echo $r | awk '{print $3}')    # Filesystem
 
-                # Gerät existiert, mit UUID mounten
-                echo $(grep $uuid /etc/fstab)  ..... found, mounting
-                echo mount $uuid $mF_mountpoint$mountpoint
+            echo $device
+            echo $mountpoint
+            echo $filesystem
+
+            uuid=$(echo ${device} | grep '^UUID' | cut -d = -f 2)   # Geräte mit UUID, UUID separieren
+            echo $uuid
+            if [ "$uuid" != "" ]; then
+
+                # wenn $uuid einen Wert enthält (eine UUID) -> prüfen, ob für diese UUID ein Gerät existiert
+                if [ -h /dev/disk/by-uuid/$uuid ]; then
+
+                    # Gerät existiert, mit UUID mounten
+                    echo $(grep $uuid /etc/fstab)  ..... found, mounting
+                    echo mount $uuid $mF_mountpoint$mountpoint
+                else
+
+                    # UUID geliefert aber UUID existiert nicht als Gerät -> Skip
+                    echo ""
+                fi
             else
 
-                # UUID geliefert aber UUID existiert nicht als Gerät -> Skip
-                echo ""
+                # keine UUID geliefert, sondern Gerätepfad -> Mit Gerätepfad mounten
+                echo "mount $device $mF_mountpoint$mountpoint"
             fi
-        else
+        fi
+    done <<<"$line"
+    return 0
+}
 
-            # keine UUID geliefert, sondern Gerätepfad -> Mit Gerätepfad mounten
-            echo "mount $device $mF_mountpoint$mountpoint"
+
+# =========================================================
+# Funktion:     umountFstab()
+# Aufgabe:      iteriert /etc/fstab rückwerts und 
+#               dismounted alle Einträge, die gemounted 
+#               sind
+# Parameter:    $1 Pfad zur fstab
+#               $2 Mountpoint
+# Return:       0:    Ok
+#               2001: Wenn Fehler in Parameterübergabe
+# Echo:         String
+# =========================================================
+function umountFstab {
+    # Parameter prüfen
+    if [ $# -lt 2 ]
+    then
+        log "regular" "ERROR" "FUNC: mountFstab(): Fehler bei Parameterübergabe"
+        return $rERROR_WrongParameters
+    fi
+
+    # Übergabeparameter abholen
+    uF_fstab=$1
+    uF_mountpoint=$2
+
+
+    # Alle Einträge der /etc/fstab liefern, die mit U(UID) oder /(dev/...) beginnen und Filesystem
+    # ext2, ext3 oder ext4 sind.
+    # line=$(grep ^[U\/] /etc/fstab | grep '.ext[2-4].')
+
+    # alle gemounteten Laufwerke ausgeben, Mountpoints extrahieren, Mountpoints filtern, 
+    # die den Basismountpoint enthalten und diese sortieren
+    line=$(mount | awk '{print $3}' | grep "$uFmountpoint" | sort -r)
+
+    ### Alle Einträge der /etc/fstab liefern, ausser Kommentare, swap, iso9660, udf
+    ### line=$(grep -v '^#' "$mF_fstab" | grep -v '.swap.' | grep -v 'udf' | grep -v 'iso9660')
+    
+    ### Effektiv kann die liste noch sortiert werden nach Spalte 2 (Mountpoint)
+    ### line=$(grep -v '^#' /etc/fstab | grep -v '.swap.' | grep -v 'udf' | grep -v 'iso9660' | sort -r -k 2)
+
+    # Alle Zeilen in einer Schleife durchlaufen...
+    while read entry
+    do
+
+        # Zeile ausgeben...
+        echo ''
+        echo $entry 
+
+        # $uF_mountpoint vom gefundenen Mountpoint abtrennen
+        # bsp.: $uF_mountpoint=/mnt/src ; $entry=/mnt/src/home ; bleibt: /home
+        # ${uF_Mountpoint}+1 = Offset
+        # ${entry:OFFSET} = liefert $entry ab dem Offset, also den Gesamtmountpoint ohne den Basismountpoint
+        mp=${entry:${#uF_mountpoint}+1}
+
+        # Prüfen, ob es für den Mountpoint einen Eintrag in der fstab gibt
+        # liefere die Zeile aus der fstab, in der der extrahierte Mountpoint ($mp) gefunden wird
+        r=$(grep "$mp " "$uF_fstab")
+
+        # Prüfen, ob $r kein leerer String ist
+        if [ "r" != "" ]; then
+
+            umount "$mp"
         fi
     done <<<"$line"
     return 0
